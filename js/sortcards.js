128d2f356a9cabc6d3386758673ba08e740e6891
Adina Badulescu <adina.artene@yahoo.com>
2022-12-21 13:59:33 +0200

    filter templates, done

--- js/sortcards.js
+++ js/sortcards.js
@@ -1,24 +1,35 @@
 // this overrides `contains` to make it case insenstive
-// jQuery.expr[':'].contains = function(a, i, m) {
-//     console.log("m"+ m);
-//     return jQuery(a).text().toUpperCase()
-//         .indexOf(m[3].toUpperCase()) >= 0;
-//   };
-  
+jQuery.expr[':'].contains = function(a, i, m) {    
+    return jQuery(a).text().toUpperCase()
+        .indexOf(m[3].toUpperCase()) >= 0;
+  };
+
+$('#ricercas').keyup(function () {
+  $('div.card-body.col-sm-4.sm-margin-b-50').removeClass('hidden-custom');
+  var filter = $(this).val(); // get the value of the input, which we filter on
+  console.log("filter " + filter);
+  $('#remove-card').find('.card  .card-body h4:not(:contains("' + filter + '"))').parent().addClass('hidden-custom');
+  console.log($('#remove-card').find('.card .card-body h4:not(:contains("' + filter + '"))').parent().addClass('hidden-custom'));
+})
+
+// col-sm-4 sm-margin-b-50
 
 
-  $('#btnSort').click(function (){
-      $('.card-deck .card').sort(function(a,b) {
-          return $(a).find(".card-title").text() > $(b).find(".card-title").text() ? 1 : -1;
-      }).appendTo(".card-deck");
-  })
 
 
 
-  $('#ricercas').keyup(function (){   
-      $('.card').removeClass('d-none');
-      var filter = $(this).val(); // get the value of the input, which we filter on
-      console.log("filter " + filter);
-      $('.remove-card').find('.card .card-body h4 a:not(:contains("'+filter+'"))').parent().parent().addClass('d-none');
-  })
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
