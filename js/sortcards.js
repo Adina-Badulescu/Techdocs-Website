389498a94d9135a5142fce14c79b8fc9d5424215
Adina Badulescu <adina.artene@yahoo.com>
2022-12-21 17:18:28 +0200

    template id sent in URL to the contact form

--- js/sortcards.js
+++ js/sortcards.js
@@ -9,8 +9,28 @@ $('#ricercas').keyup(function () {
   var filter = $(this).val(); // get the value of the input, which we filter on
   console.log("filter " + filter);
   $('#remove-card').find('.card  .card-body h4:not(:contains("' + filter + '"))').parent().addClass('hidden-custom');
-  console.log($('#remove-card').find('.card .card-body h4:not(:contains("' + filter + '"))').parent().addClass('hidden-custom'));
+  // console.log($('#remove-card').find('.card .card-body h4:not(:contains("' + filter + '"))').parent().addClass('hidden-custom'));
+  
 })
+
+formUrl = "./contact_form/form.html"
+let templateId;
+let templates = window.document.getElementsByClassName("link");
+
+
+function getTemplateId(e) {  
+  templateId = e.target.id
+  return templateId;
+}
+for (const template of templates) {
+  template.addEventListener('mouseenter', getTemplateId)
+}
+
+function selectedTemplate() {
+  window.location.href = formUrl + "?" + "template" + "=" + templateId;
+}
+
+
 
 // col-sm-4 sm-margin-b-50
 
